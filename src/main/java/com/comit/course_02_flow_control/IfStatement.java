90a5c1d6de6003b43fa31c041fb0905d63e5f503
Gopal Sharma <gopalenggr@gmail.com>
2023-02-14 20:05:49 -0600

    latest one

--- src/main/java/com/comit/course_02_flow_control/IfStatement.java
+++ src/main/java/com/comit/course_02_flow_control/IfStatement.java
@@ -1,23 +1,58 @@
 package com.comit.course_02_flow_control;
 
 public class IfStatement {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		int a = 20;
 		int b = 20;
-		
-		if(a>b)
-		{
+
+		// Scenario 1
+		if (a > b) {
+			System.out.println("a is greater than b");
+		} else if (a < b) {
+			System.out.println(" a is less than b");
+		} else {
+			System.out.println(" a is equal to b");
+		}
+
+		// 2nd Scenario
+		if (a > b) {
 			System.out.println("a is greater than b");
 		}
-		else if(a<b)
-		{
+		if (a < b) {
 			System.out.println(" a is less than b");
+		}
+		if (a == b) {
+			System.out.println(" a is equal to b");
+		}
+
+		// 3rd Scenario --nested if
+
+		int c = 25;
+		if (a > b) {
+			if (a < c) {
+
+			}
+		}
+		
+		
+		String str;
+		a=10;
+		b=15;
+		if(a>b)
+		{ 
+			str = "a is greater than b";
+			
 		}
 		else
 		{
-			System.out.println(" a is equal to b");
+			str = "a is less than b";
 		}
+		
+		// Ternary Operator
+		str = (a>b)? "a is greater than b" : "a is less than b";
+		System.out.println(str);
+		
+		
 	}
 
 }
